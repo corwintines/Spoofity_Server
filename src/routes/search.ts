846be3a888e9fe4621754f804820790670a16319
Tyler Stewart <hello@tylerstewart.ca>
2019-07-26 22:20:48 -0600

    feat: create playlist on login

--- src/routes/search.ts
+++ src/routes/search.ts
@@ -1,5 +1,20 @@
 import { RequestHandler } from 'express';
+import { query } from '../services/database';
 
 export const search: RequestHandler = async (req, res) => {
   const { room, q, offset, limit } = req.query;
+
+  const auth = await query(
+    `
+    SELECT
+      auth.service,
+      auth.token,
+      auth.token_type,
+      auth.refresh_token
+    FROM playlist
+      JOIN auth USING (auth_id)
+    WHERE playlist.room_code = $1
+  `,
+    [room]
+  );
 };
--- src/routes/spotify/login.ts
+++ src/routes/spotify/login.ts
@@ -1,6 +1,8 @@
 import { RequestHandler } from 'express';
-import { createSpotifyToken, getSpotifyUser } from '../../services/spotify';
+import { createSpotifyToken } from '../../services/spotify';
 import { CLIENT_URL } from '../../const';
+import { transaction, query } from '../../services/database';
+import { generateRoomCode } from '../../services/database/room';
 
 export const spotifyLogin: RequestHandler = async (req, res) => {
   const { code, state } = req.query;
@@ -11,19 +13,40 @@ export const spotifyLogin: RequestHandler = async (req, res) => {
     // Get spotify auth token
     const token = await createSpotifyToken(code);
 
-    // Get spotify user
-    const user = await getSpotifyUser({
-      token: token.access_token,
-      tokenType: token.token_type
+    // Store auth
+    await transaction(async (client) => {
+      await query(
+        `
+        INSERT INTO auth (
+          service,
+          token,
+          token_type,
+          refresh_token,
+          expiry_date
+        ) VALUES (
+          'spotify',
+          $1,
+          $2,
+          $3,
+          now() + 'interval $4 seconds'
+        );
+      `,
+        [
+          token.access_token,
+          token.token_type,
+          token.refresh_token,
+          token.expires_in
+        ],
+        client
+      );
     });
 
-    // TODO: store user
-
-    // TODO: generate playlist code
+    // Generate room code
     // This will eventually be moved to it's own route
+    //  with the intoduction of a playlist manager
+    const roomCode = await generateRoomCode();
 
-    const roomCode = 'A12B';
-
+    // Redirect user to the playlist
     res.redirect(302, `${CLIENT_URL}/${roomCode}`);
   } catch (err) {
     // TODO: this should probably redirect the user to an error page in the client
