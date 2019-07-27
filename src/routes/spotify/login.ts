86a5304cbd9da0db02237381605eb68091d83f4f
Tyler Stewart <hello@tylerstewart.ca>
2019-07-27 10:40:51 -0600

    feat: update routes

--- src/routes/spotify/login.ts
+++ src/routes/spotify/login.ts
@@ -1,8 +1,11 @@
 import { RequestHandler } from 'express';
-import { createSpotifyToken } from '../../services/spotify';
+import {
+  createSpotifyToken,
+  createSpotifyPlaylist,
+  getSpotifyUser
+} from '../../services/spotify';
 import { CLIENT_URL } from '../../const';
-import { transaction, query } from '../../services/database';
-import { generateRoomCode } from '../../services/database/room';
+import { transaction, querySingle } from '../../services/database';
 
 export const spotifyLogin: RequestHandler = async (req, res) => {
   const { code, state } = req.query;
@@ -13,9 +16,9 @@ export const spotifyLogin: RequestHandler = async (req, res) => {
     // Get spotify auth token
     const token = await createSpotifyToken(code);
 
-    // Store auth
-    await transaction(async (client) => {
-      await query(
+    const roomCode = await transaction(async (client) => {
+      // Store auth
+      const auth = await querySingle(
         `
         INSERT INTO auth (
           service,
@@ -29,7 +32,8 @@ export const spotifyLogin: RequestHandler = async (req, res) => {
           $2,
           $3,
           now() + 'interval $4 seconds'
-        );
+        )
+        RETURNING auth_id;
       `,
         [
           token.access_token,
@@ -39,12 +43,55 @@ export const spotifyLogin: RequestHandler = async (req, res) => {
         ],
         client
       );
-    });
+      if (!auth) {
+        throw new Error('Failure creating authorization entry');
+      }
 
-    // Generate room code
-    // This will eventually be moved to it's own route
-    //  with the intoduction of a playlist manager
-    const roomCode = await generateRoomCode();
+      // Generate room code
+      const roomCode = await querySingle(
+        'SELECT generateUniqueRoomCode FROM generateUniqueRoomCode(4)',
+        undefined,
+        client
+      );
+      if (!roomCode) {
+        throw new Error('Failure creating room code');
+      }
+
+      // Grab the user from spotify
+      const user = await getSpotifyUser({
+        token: token.access_token,
+        tokenType: token.token_type
+      });
+
+      // Create a spotify playlist
+      const playlist = await createSpotifyPlaylist({
+        roomCode,
+        token: token.access_token,
+        tokenType: token.token_type,
+        userId: user.id
+      });
+
+      // Create a room
+      // This will eventually be moved to it's own route
+      //  with the intoduction of a playlist manager
+      await querySingle(
+        `
+        INSERT INTO room (
+          auth_id,
+          room_code,
+          service_playlist_id
+        ) VALUES (
+          $1,
+          $2,
+          $3
+        )
+      `,
+        [auth.auth_id, roomCode, playlist.id],
+        client
+      );
+
+      return roomCode;
+    });
 
     // Redirect user to the playlist
     res.redirect(302, `${CLIENT_URL}/${roomCode}`);
