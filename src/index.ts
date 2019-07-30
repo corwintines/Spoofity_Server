e643eb5cc18fbe1ae4abc3c61df2d241affd4628
Tyler Stewart <hello@tylerstewart.ca>
2019-07-29 20:52:53 -0600

    fix: add cors

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
 import * as express from 'express';
+import * as cors from 'cors';
 import { SERVER_PORT } from './const';
 import registerRoutes from './routes';
 
 const app = express();
+app.use(
+  cors({
+    origin: '*'
+  })
+);
+
 registerRoutes(app);
+
 app.listen(SERVER_PORT, () => {
   console.log('Listening on port', SERVER_PORT);
 });
