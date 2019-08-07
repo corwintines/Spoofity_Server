dd975791f201c200026d792fd75f36975a0b1cf2
Tyler Stewart <hello@tylerstewart.ca>
2019-08-06 22:17:39 -0600

    feat(db): support users with multiple playlists
    `user_profile` indicates a user of the app
    
    They can connect one or many music services through `user_service`
    
    That service's authorization tokens are stored in `user_auth`
    
    Each playlist is linked to a `user_auth`
    
    `user_profile` 1->M `user_service` 1->1 `user_auth`
                                       1->M `playlist`

--- database/init.sql
+++ database/init.sql
@@ -12,19 +12,36 @@ CREATE TABLE request (
 
 CREATE INDEX request_request_id_idx ON request (request_id);
 
-CREATE TABLE auth (
-  auth_id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+CREATE TABLE user_profile (
+  user_profile_id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+  email text NOT NULL
+);
+CREATE INDEX user_profile_user_profile_id_idx ON user_profile (user_profile_id);
+
+
+CREATE TABLE user_service (
+  user_service_id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+  user_profile_id uuid NOT NULL REFERENCES user_profile(user_profile_id),
   service music_service NOT NULL,
+  service_user_data jsonb NOT NULL
+);
+CREATE INDEX user_service_user_service_id_idx ON user_service (user_service_id);
+CREATE INDEX user_service_user_profile_id_idx ON user_service (user_profile_id);
+
+CREATE TABLE user_auth (
+  user_auth_id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+  user_service_id uuid NOT NULL REFERENCES user_service(user_service_id),
   token text NOT NULL,
   token_type text NOT NULL,
   refresh_token text NOT NULL,
-  created_date timestamptz NOT NULL NOT NULL DEFAULT now(),
+  created_date timestamptz NOT NULL DEFAULT now(),
   expires_in integer NOT NULL
 );
 
-CREATE INDEX auth_auth_id_idx ON auth (auth_id);
+CREATE INDEX user_auth_user_auth_id_idx ON user_auth (user_auth_id);
+CREATE INDEX user_auth_user_service_id_idx ON user_auth (user_service_id);
 
-CREATE OR REPLACE FUNCTION generate_unique_room_code(arg_length integer) 
+CREATE OR REPLACE FUNCTION generate_unique_playlist_code(arg_length integer) 
   RETURNS text
 AS $$
 DECLARE
@@ -41,8 +58,8 @@ BEGIN
     -- Check to make sure the code is unique
     SELECT NOT EXISTS(
       SELECT true
-      FROM room
-      WHERE room_code = generated_code
+      FROM playlist
+      WHERE playlist_code = generated_code
     ) INTO is_unique;
     EXIT WHEN is_unique;
   END LOOP;
@@ -51,31 +68,33 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql STABLE;
 
-CREATE TABLE room (
-  room_id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
-  auth_id uuid NOT NULL REFERENCES auth(auth_id),
-  room_code text UNIQUE NOT NULL,
-  service_data jsonb NOT NULL,
+CREATE TABLE playlist (
+  playlist_id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+  user_auth_id uuid NOT NULL REFERENCES user_auth(user_auth_id),
+  playlist_name text NOT NULL,
+  playlist_code text UNIQUE NOT NULL,
+  service_playlist_data jsonb NOT NULL,
   created_date timestamptz NOT NULL DEFAULT now()
 );
 
-CREATE INDEX room_room_id_idx ON room (room_id);
-CREATE INDEX room_room_code_idx ON room (room_code);
+CREATE INDEX playlist_playlist_id_idx ON playlist (playlist_id);
+CREATE INDEX playlist_user_auth_id_idx ON playlist (user_auth_id);
+CREATE INDEX playlist_playlist_code_idx ON playlist (playlist_code);
 
-CREATE TABLE room_setting (
-  room_id uuid PRIMARY KEY DEFAULT uuid_generate_v4()
+CREATE TABLE playlist_setting (
+  playlist_id uuid PRIMARY KEY DEFAULT uuid_generate_v4()
 );
 
-CREATE TABLE room_setting_voting (
+CREATE TABLE playlist_setting_voting (
   voting_enabled boolean NOT NULL DEFAULT false,
   voting_time_secs integer NOT NULL DEFAULT 30
-) INHERITS (room_setting);
+) INHERITS (playlist_setting);
 
 
 CREATE TABLE track (
   track_id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
-  room_id uuid NOT NULL REFERENCES room(room_id),
-  service_data jsonb NOT NULL,
+  playlist_id uuid NOT NULL REFERENCES playlist(playlist_id),
+  service_track_data jsonb NOT NULL,
   created_date timestamptz NOT NULL DEFAULT now()
 );
 
